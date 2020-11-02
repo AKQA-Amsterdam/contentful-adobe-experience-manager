720f78ee3d024da8563026d1fb008907d5773687
Marcos Mellado <marcos.mellado@akqa.com>
2020-11-02 11:49:30 +0100

    Initial development setup

--- src/index.tsx
+++ src/index.tsx
@@ -4,22 +4,16 @@ import { render } from 'react-dom';
 import {
   AppExtensionSDK,
   FieldExtensionSDK,
-  SidebarExtensionSDK,
   DialogExtensionSDK,
-  EditorExtensionSDK,
-  PageExtensionSDK,
   BaseExtensionSDK,
   init,
-  locations
+  locations,
 } from 'contentful-ui-extensions-sdk';
 import '@contentful/forma-36-react-components/dist/styles.css';
 import '@contentful/forma-36-fcss/dist/styles.css';
 import './index.css';
 
 import Config from './components/ConfigScreen';
-import EntryEditor from './components/EntryEditor';
-import Page from './components/Page';
-import Sidebar from './components/Sidebar';
 import Field from './components/Field';
 import Dialog from './components/Dialog';
 
@@ -32,28 +26,16 @@ init((sdk: BaseExtensionSDK) => {
   const ComponentLocationSettings = [
     {
       location: locations.LOCATION_APP_CONFIG,
-      component: <Config sdk={(sdk as unknown) as AppExtensionSDK} />
+      component: <Config sdk={(sdk as unknown) as AppExtensionSDK} />,
     },
     {
       location: locations.LOCATION_ENTRY_FIELD,
-      component: <Field sdk={(sdk as unknown) as FieldExtensionSDK} />
-    },
-    {
-      location: locations.LOCATION_ENTRY_EDITOR,
-      component: <EntryEditor sdk={(sdk as unknown) as EditorExtensionSDK} />
+      component: <Field sdk={(sdk as unknown) as FieldExtensionSDK} />,
     },
     {
       location: locations.LOCATION_DIALOG,
-      component: <Dialog sdk={(sdk as unknown) as DialogExtensionSDK} />
+      component: <Dialog sdk={(sdk as unknown) as DialogExtensionSDK} />,
     },
-    {
-      location: locations.LOCATION_ENTRY_SIDEBAR,
-      component: <Sidebar sdk={(sdk as unknown) as SidebarExtensionSDK} />
-    },
-    {
-      location: locations.LOCATION_PAGE,
-      component: <Page sdk={(sdk as unknown) as PageExtensionSDK} />
-    }
   ];
 
   // Select a component depending on a location in which the app is rendered.
@@ -61,7 +43,7 @@ init((sdk: BaseExtensionSDK) => {
   // NB: Location "app-config" is auto-included in the list as most apps need it
   // You can remove it (and on the app definition also) in case the app
   // doesn't require it
-  ComponentLocationSettings.forEach(componentLocationSetting => {
+  ComponentLocationSettings.forEach((componentLocationSetting) => {
     if (sdk.location.is(componentLocationSetting.location)) {
       render(componentLocationSetting.component, root);
     }
